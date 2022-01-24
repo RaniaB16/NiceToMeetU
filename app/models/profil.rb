17a02d31b8980213a17c1aa38299e2ec72110976
Rania BOUTEKRABET <raniabtkt@gmail.com>
2022-01-25 00:08:50 +0100

    add validation

--- app/models/profil.rb
+++ app/models/profil.rb
@@ -1,2 +1,7 @@
 class Profil < ApplicationRecord
+  validates :user_id, presence: true
+  validates :full_name, presence: true
+  validates :actual_job, presence: true
+  validates :company, presence: true
+  validates :description, presence: true
 end

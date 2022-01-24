32fb0d7d3cdc8e36e1a0d69905b4d669e12b22f7
Rania BOUTEKRABET <raniabtkt@gmail.com>
2022-01-25 00:00:36 +0100

    adding Profil model and test

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_24_214914) do
+ActiveRecord::Schema.define(version: 2022_01_24_224540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profils", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "full_name"
+    t.string "actual_job"
+    t.string "company"
+    t.text "description"
+    t.string "dream_job"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false

386c777fc384902ac4759e325b19743d7d8761e0
Rania BOUTEKRABET <raniabtkt@gmail.com>
2022-05-02 18:35:14 +0200

    finishing CRUD

--- app/controllers/profils_controller.rb
+++ app/controllers/profils_controller.rb
@@ -14,20 +14,39 @@ class ProfilsController < ApplicationController
   end
 
   def create
+    # Profil est un super params créé par rails
+    # (paremetre principal diff de params inbriqué)
+    # Plutôt que récuperer les infos 1 par 1
+    # On appelle les params de Profil en récupèrant le hash
+    # On appelle uniquement les strong params pour éviter ajout de tout autre
+    # cf PRIVATE ci-dessous
+    Profil.create(profil_params)
+    # Pour eviter de creer template Création. Rediriger
+    redirect_to profils_path
   end
 
   def edit
+    @profil = Profil.find_by(user_id: current_user.id)
   end
 
   def update
+    @profil = Profil.find_by(user_id: current_user.id)
+    @profil.update(profil_params)
+    redirect_to profils_path
+
   end
 
   def destroy
+    @profil = Profil.find_by(user_id: current_user.id)
+    @profil.destroy
+    redirect_to profils_path
   end
 
   private
 
   def profil_params
-    params.require(:profil).permit(:user_id, :full_name, :actual_job, :company, :description, :dream_job)
+    # Syntax à respecter
+    params.require(:profil).permit(:full_name, :actual_job, :company, :description, :dream_job)
   end
+
 end
